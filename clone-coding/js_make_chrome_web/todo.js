9dd8c767e151bc7983dd190a98d64a5783086d4c
top7578 <39055981+top7578@users.noreply.github.com>
2021-04-21 23:31:38 +0900

    Update todo.js

--- clone-coding/js_make_chrome_web/todo.js
+++ clone-coding/js_make_chrome_web/todo.js
@@ -1,18 +1,36 @@
+//1. localStorage에는 js data 저장 불가 (string 형태로만 저장), JSON.stringify를 통해서 js object를 string으로 변환
+//2. string에서 js object로 변환
+//3. array에 담겨 있는 것들 각각에 한번씩 함수를 실행
+
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS ='toDos';
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //1
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerHTML = "X";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerHTML = "X";  
   span.innerText = text;
   li.appendChild(delBtn)
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  }
+  toDos.push(toDoObj)
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,9 +41,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if(toDos !== null) {
-
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos); //2
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    }); //3
   }
 }
 
