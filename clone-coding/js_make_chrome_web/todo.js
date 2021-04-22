f882406bb8c49e9752718b79d88dd270e82d4835
top7578 <39055981+top7578@users.noreply.github.com>
2021-04-22 09:55:34 +0900

    Update todo.js

--- clone-coding/js_make_chrome_web/todo.js
+++ clone-coding/js_make_chrome_web/todo.js
@@ -1,6 +1,8 @@
 //1. localStorage에는 js data 저장 불가 (string 형태로만 저장), JSON.stringify를 통해서 js object를 string으로 변환
 //2. string에서 js object로 변환
 //3. array에 담겨 있는 것들 각각에 한번씩 함수를 실행
+//4. filter은 array에 담겨 잇는 것들 참인 것 return 해서 새로운 array 반환
+//5. parseInt: string -> int
 
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
@@ -8,7 +10,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS ='toDos';
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {  //4
+    return toDo.id !== parseInt(li.id); //5
+  });
+  toDos = cleanToDos
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //1
@@ -20,6 +33,7 @@ function paintToDo(text) {
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerHTML = "X";  
+  delBtn.addEventListener("click", deleteToDo)
   span.innerText = text;
   li.appendChild(delBtn)
   li.appendChild(span);
@@ -28,7 +42,7 @@ function paintToDo(text) {
   const toDoObj = {
     text: text,
     id: newId
-  }
+  };
   toDos.push(toDoObj)
   saveToDos();
 }
